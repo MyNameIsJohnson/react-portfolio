7ca61d46a7154cb2f92ce0418171ef27ae985b99
johnsonthieu <johnsonle2929@gmail.com>
2022-03-31 22:04:05 -0700

    test commit

---
+++ src/data.js
@@ -0,0 +1,62 @@
+export const projects = [
+  {
+    title: "DiceRacer",
+    subtitle: "HTML, CSS, Javascript Stack",
+    description:
+      "This was my first project at General Assembly. I used HTML, CSS and Javascript to complete this project. Since then, I've learned new skills that I plan on updated this project with.",
+    image: "./diceracer.gif",
+    link: "https://mynameisjohnson.github.io/Dice-Racers/",    
+  },
+  {
+    title: "Club Hoppin'",
+    subtitle: "Python, MongoDb and Express",
+    description:
+      "For this porject, I used Python, MongoDb and Express.js. This was a fun project that I worked on the frontend and collaborated on the completing the backend.",
+    image: "./clubhoppin.gif",
+    link: "https://clubhoppin.herokuapp.com/",
+  },
+  {
+    title: "Pikatsume",
+    subtitle: "Python and Django",
+    description:
+      "Built using HTML, CSS and Javascript, Python, Django that allows the user to create a profile, catch pikachus, and purchase poffins to catch pikachus.",
+    image: "./pikatsume.gif",
+    link: "https://pikatsume.herokuapp.com/",
+  },
+  {
+    title: "TLM Replica",
+    subtitle: "React",
+    description:
+      "This was built using React and grid layout. This was intended to replicate an existing website to highlight my React and CSS skills.",
+    image: "./tlm.gif",
+    link: "https://thelastmile-replica.netlify.app/",
+  },
+];
+
+export const testimonials = [
+  {
+    quote:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa quod necessitatibus quidem impedit facilis? Distinctio laboriosam veritatis recusandae minus. Voluptatum accusantium hic laudantium soluta ea sapiente cupiditate officia maiores itaque.",
+    image: "https://randomuser.me/api/portraits/men/1.jpg",
+    name: "Jesse Hicks",
+    company: "Zoozle",
+  },
+  {
+    quote:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa quod necessitatibus quidem impedit facilis? Distinctio laboriosam veritatis recusandae minus. Voluptatum accusantium hic laudantium soluta ea sapiente cupiditate officia maiores itaque.",
+    image: "https://randomuser.me/api/portraits/men/94.jpg",
+    name: "Ruben Alvarez",
+    company: "Dooble.io",
+  },
+];
+
+export const skills = [
+  "JavaScript",
+  "React",
+  "Python",
+  "Node",
+  "MongoDb",
+  "Express",
+  "Django",
+  "CSS",
+];
