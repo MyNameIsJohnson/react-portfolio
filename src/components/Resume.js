a0a20b1292c2be54398d8a60d1550ead6d49b668
johnson <johnsonle2929@gmail.com>
2022-08-09 16:28:15 -0700

    deleted unused file

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -1,5 +1,4 @@
 import React from "react";
-import "./Resume.module.css";
 import { Page, Document, pdfjs } from "react-pdf";
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
 
