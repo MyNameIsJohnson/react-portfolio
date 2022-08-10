06054b2a2fc744ec0cb4e2b40a7277fc4bb6780d
johnson <johnsonle2929@gmail.com>
2022-08-10 08:55:54 -0700

    update

--- craco.config.js
+++ craco.config.js
@@ -1,10 +1,8 @@
 module.exports = {
   style: {
     postcss: {
-      plugins: [
-        require('tailwindcss'),
-        require('autoprefixer'),
-      ],
+      plugins: [require("tailwindcss"), require("autoprefixer")],
     },
   },
-}
+  eslint: { enable: false },
+};
