775179f33352ea5fd004658d0f23f2afce4a0ba8
johnson <johnsonle2929@gmail.com>
2022-08-10 08:53:11 -0700

    Updates

---
+++ precache-manifest.4eb2d7187962f428af7b159be73bed03.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "67896f88ddf67ed39176f10ff92b3028",
+    "url": "/react-portfolio/index.html"
+  },
+  {
+    "revision": "a451b27ad93983de16cd",
+    "url": "/react-portfolio/static/css/main.99105844.chunk.css"
+  },
+  {
+    "revision": "2f8c34f6ff9b9219b4ca",
+    "url": "/react-portfolio/static/js/2.5f8dc0c2.chunk.js"
+  },
+  {
+    "revision": "4cdd5a02e1c14114da0db478f4ddb328",
+    "url": "/react-portfolio/static/js/2.5f8dc0c2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a451b27ad93983de16cd",
+    "url": "/react-portfolio/static/js/main.a84d3f8d.chunk.js"
+  },
+  {
+    "revision": "0324b683af6ac71e8ab6",
+    "url": "/react-portfolio/static/js/runtime-main.a33726c6.js"
+  }
+]);
